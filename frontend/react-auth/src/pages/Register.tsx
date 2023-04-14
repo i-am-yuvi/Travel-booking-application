fca54b603a3fd650237e51c8d5342c25f5d81df5
Yuvraj Chhetri <yuvichh01@gmail.com>
2023-04-14 21:00:21 +0530

    updated the login

--- frontend/react-auth/src/pages/Register.tsx
+++ frontend/react-auth/src/pages/Register.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+
 
 const Register = () =>{
+
+     const[name, setname] = useState('');
+     const[email, setEmail] = useState('');
+     const[password, setPass] = useState('');
+
+
+     const submit = async (e: SyntheticEvent) => {
+          e.preventDefault();
+
+          await fetch('http://localhost:8000/api/register', {
+               method:'POST',
+               headers: {'Content-Type': 'application/json'}, 
+               body: JSON.stringify({
+                    name,
+                    email,
+                    password
+               })
+          });
+          
+     }
+
+
      return(
-          <div>
-               Register
-          </div>
+          <form>
+               <h1 className="h3 mb-3 fw-normal">Please Register</h1>
+
+               <input type="name" className="form-control" id="floatingInput" placeholder="Name"/>
+               <br />
+               <input type="emailID" className="form-control" id="floatingInput" placeholder="example@napptive.com"/>
+               <br />
+               <input type="passwd" className="form-control" id="floatingInput" placeholder="Password"/>
+               <br />
+               <input type="passwd" className="form-control" id="floatingInput" placeholder="Confirm Password"/>
+               <br/>
+
+               <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+          </form>
      );
 };
 
 export default Register;
+
+
--- frontend/react-auth/src/pages/login.tsx
+++ frontend/react-auth/src/pages/login.tsx
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () =>{
-     return(
-          <form>
-               <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-               <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-               <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+  const navigate = useNavigate();
 
-               <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-          </form>
-     );
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:8000/api/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    
+    if (response.ok) {
+      navigate('/home');
+    }
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+      <input
+        type="email"
+        className="form-control"
+        id="floatingInput"
+        placeholder="name@example.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        className="form-control"
+        id="floatingPassword"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button className="w-100 btn btn-lg btn-primary" type="submit">
+        Sign in
+      </button>
+    </form>
+  );
 };
 
 export default Login;
-
-
