658f7b346c82aa39ae0d670409e0cdf117d63c16
Yuvraj Chhetri <yuvichh01@gmail.com>
2023-04-14 23:59:52 +0530

    updated

--- frontend/react-auth/src/pages/Home.tsx
+++ frontend/react-auth/src/pages/Home.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Home = () =>{
+
+     const [place, setPlace] = useState('');
+     const [arrivalDate, setArrivalDate] = useState('');
+     const [departureDate, setDepartureDate] = useState('');
+     const [hotelResults, setHotelResults] = useState([]);
+
+     const handleSubmit = async (event: { preventDefault: () => void; }) => {
+     event.preventDefault();
+          // Call Amadeus API here
+     };
+
      return(
           <div>
-               Home
+               <h1>Home Page</h1>
+               <form onSubmit={handleSubmit}>
+                    <br/>
+                    <input type="text" placeholder="Place of visit" value={place} onChange={(e) => setPlace(e.target.value)}/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <p>Date of Visit:</p>
+                    <input type="date" value={arrivalDate} onChange={(e) => setArrivalDate(e.target.value)}/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <p>Date of Leave:</p>
+                    <input type="date" value={departureDate} onChange={(e) => setDepartureDate(e.target.value)}/>
+                    <br/>
+                    <br/>
+                    <button type="submit">Search Hotels</button>
+                    <br/>
+               </form>
+          {/* Render hotelResults here */}
           </div>
      );
 };
